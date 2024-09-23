0d1647f8017524af767742438df598ca4fb73254
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2024-09-23 18:02:00 -0500

    Begin HU7

---
+++ src/main/java/com/EmazonPragma/EmazonUser_Pragma/EmazonUserPragmaApplication.java
@@ -0,0 +1,13 @@
+package com.EmazonPragma.EmazonUser_Pragma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonUserPragmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonUserPragmaApplication.class, args);
+	}
+
+}
