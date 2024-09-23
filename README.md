9a6bbee09a3af72806b3063f506145f31eca9625
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2024-09-23 16:47:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EmazonUser_Pragma
+ 
---
+++ src/test/java/com/EmazonPragma/EmazonUser_Pragma/EmazonUserPragmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmazonPragma.EmazonUser_Pragma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonUserPragmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
